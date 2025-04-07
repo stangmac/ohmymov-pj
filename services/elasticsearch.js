a8bb83cea9c0112e0b200a317d11a18615638dde
kantinunpaly <palykantinun2002@gmail.com>
2025-04-07 19:51:50 +0700

    ค้นหาๆ

--- services/elasticsearch.js
+++ services/elasticsearch.js
@@ -3,7 +3,7 @@ const { Client } = require('@elastic/elasticsearch');
 const client = new Client({
   node: 'http://localhost:9200', // เปลี่ยนเป็น URL ของ Elasticsearch Server ของคุณ
   auth: {
-    username: 'elasticsearch', // เปลี่ยนเป็นของคุณ
+    username: 'elastic', // เปลี่ยนเป็นของคุณ
     password: 'yourpassword'
   }
 });
