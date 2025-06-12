73e8553ce12e666db1f51260a5505792d85dbce8
kantinunpaly <palykantinun2002@gmail.com>
2025-06-12 15:07:06 +0700

    bycyp

--- controllers/changePasswordController.js
+++ controllers/changePasswordController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const sendEmail = require("../services/mailService");
 
 // ✅ ฟังก์ชันขอ OTP
